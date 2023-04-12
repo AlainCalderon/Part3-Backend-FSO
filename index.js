6c793f9ba4958a673cc291afeeb644414cecdb60
Alain Calderon <ajcalderon171999@gmail.com>
2023-04-13 02:42:10 +0800

    Added validations and update/delete functionality

--- index.js
+++ index.js
@@ -1,48 +1,49 @@
+//all imports start here
 require("dotenv").config();
-console.log(process.env);
+
 const express = require("express");
 const app = express();
 const Person = require("./models/person");
 const morgan = require("morgan");
 const cors = require("cors");
 
+//Start of code
+app.use(express.static("build"));
+app.use(express.json());
 morgan.token("postData", (req, res) => {
   return JSON.stringify(req.body);
 });
 
-app.use(express.json());
 app.use(
   morgan(
     ":method :url :status :res[content-length] - :response-time ms :postData "
   )
 );
 app.use(cors());
-app.use(express.static("build"));
+
 const PORT = process.env.PORT || 3001;
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
+//Error handlers
+const idError = (err, req, res, next) => {
+  console.log(err.message);
+  if (err.name === "CastError") {
+    return res.status(400).send({ error: "Problem with ID" });
+  } else if (err.name === "ValidationError") {
+    return res.status(400).send({ error: err.message });
+  }
+  next(err);
+};
 
+app.use(idError);
+
+const errorHandler = (err, req, res, next) => {
+  console.log(err.message);
+  res.status(500).end();
+};
+
+app.use(errorHandler);
+
+//Routes
 app.get("/api/persons", (req, res) => {
   Person.find({})
     .then((personData) => {
@@ -52,35 +53,61 @@ app.get("/api/persons", (req, res) => {
 });
 
 app.get("/info", (req, res) => {
-  let count = persons.length;
   const date = new Date();
-  res.send(`<p>Phonebook has ${count} people<br>${date} </p>`);
+  Person.find({}).then((personData) => {
+    let count = personData.length;
+    res.send(`<p>Phonebook has ${count} people<br>${date} </p>`);
+  });
 });
-
-const generateId = () => {
-  return Math.floor(Math.random() * 1000000);
-};
 
 app.post("/api/persons", (req, res) => {
   const body = req.body;
-
-  Person.create({ name: body.name, number: parseInt(body.number) });
+  Person.create({ name: body.name, number: parseInt(body.number) })
+    .then((result) => {
+      console.log(result);
+    })
+    .catch((err) => next(err));
 });
 
 app.get("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const person = persons.find((elem) => elem.id === id);
-  if (person) {
-    res.json(person);
-  } else {
-    res.status(404).end();
-  }
+  const id = req.params.id;
+  Person.findById(id)
+    .then((personId) => {
+      res.json(personId);
+    })
+    .catch((err) => next(err));
 });
 
 app.delete("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  persons = persons.filter((elem) => elem.id !== id);
-  res.status(204).end();
+  const id = req.params.id;
+  Person.findByIdAndRemove(id)
+    .then((result) => {
+      console.log(`Deleted ${id}`);
+      res.status(204).end();
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+app.put("/api/persons/:id", (req, res) => {
+  const id = req.params.id;
+  console.log(req.body);
+  let newNumber = {
+    name: req.body.name,
+    number: req.body.number,
+  };
+
+  Person.findByIdAndUpdate(id, newNumber, {
+    runValidators: true,
+    context: "query",
+  })
+    .then((result) => {
+      res.json(result);
+    })
+    .catch((err) => {
+      next(err);
+    });
 });
 
 app.listen(PORT, () => {
--- models/person.js
+++ models/person.js
@@ -1,21 +1,23 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const uri = process.env.MONGODB_URI
+const uri = process.env.MONGODB_URI;
 
-mongoose.connect(uri).then(res => console.log("Conneted to DB")).catch(err => console.log(err.message));
+mongoose
+  .connect(uri)
+  .then((res) => console.log("Conneted to DB"))
+  .catch((err) => console.log(err.message));
 
 const personSchema = new mongoose.Schema({
-    name:String,
-    number:Number
-})
+  name: { type: String, minLength: 3 },
+  number: Number,
+});
 
+personSchema.set("toJSON", {
+  transform: (doc, returnObject) => {
+    returnObject.id = returnObject._id.toString();
+    delete returnObject._id;
+    delete returnObject.__v;
+  },
+});
 
-personSchema.set('toJSON',{
-    transform: (doc,returnObject) =>{
-        returnObject.id = returnObject._id.toString();
-        delete returnObject._id;
-        delete returnObject.__v
-    }
-})
-
-module.exports = mongoose.model('Person',personSchema);
+module.exports = mongoose.model("Person", personSchema);
